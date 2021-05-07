c60b40a625a9e05dc08b95ad6d45b5741f5f9f24
Denis <denis.tojaga@edu.fit.ba>
2021-05-07 10:45:41 +0200

    //styling the welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 
+//components import
+import GradientButton from "react-native-gradient-buttons";
+import Spacer from "../components/Spacer";
+
 
 const WelcomeScreen = ({ navigation }) => {
 
     return (
-        <View>
-            <Image style={styles.image} source={require("../images/welcome_img.png")} />
-            <Button title="Sign in" onPress={() => navigation.navigate("Signin")} />
-            <Button title="Sign up" onPress={() => navigation.navigate("Signup")} />
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require("../images/welcome.png")} />
+            </View>
+
+            <View style={styles.form}>
+                <Text style={styles.header}>WELCOME</Text>
+                <Text style={styles.header}>to the Auxilium App</Text>
+                <Button title="Sign in" onPress={() => navigation.navigate("Signin")} />
+                <Button title="Sign up" onPress={() => navigation.navigate("Signup")} />
+            </View>
+
         </View>
     );
 
@@ -17,14 +29,41 @@ const WelcomeScreen = ({ navigation }) => {
 
 
 const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: "center",
+    },
+
+    imageContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "55%",
+    },
+
     image: {
-        width: 250,
-        height: 350,
-        alignSelf: "center",
-        marginTop: 15
+        width: "88%",
+        height: "95%",
+        marginTop: 45,
+    },
+
+
+    form: {
+        borderColor: "black",
+        borderWidth: 1
     }
 
+
+
+
 });
+
+
+WelcomeScreen.navigationOptions = {
+    headerShown: false,
+    cardStyle: { backgroundColor: "white" }
+}
 
 
 
