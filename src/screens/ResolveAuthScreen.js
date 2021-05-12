8de9860419421a3cf44ac4f803132d35b4cda5d5
Denis <denis.tojaga@edu.fit.ba>
2021-05-12 17:28:08 +0200

    //fix local sign in bug

--- src/screens/ResolveAuthScreen.js
+++ src/screens/ResolveAuthScreen.js
@@ -1,33 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, ActivityIndicator } from "react-native";
 import { navigate } from "../helpers/navigation";
 import * as firebase from "firebase";
+import "firebase/firestore";
+import { signIn } from "../api/firebaseMethods";
 
 
 const ResolveAuthScreen = () => {
 
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
-
-
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
 
     useEffect(() => {
-        var isMounted = true;
+        const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
 
-        firebase.auth().onAuthStateChanged(user => {
-            if (user != null) {
-                console.log('We are authenticated now!');
-                navigate("Menu");
-            } else {
-                navigate("loginFlow");
-            }
 
-        });
+    if (initializing) return null;
 
-        return () => { isMounted = false; }
-    });
+    return (
+        <>
+            {!user ? navigate("loginFlow") : navigate("Menu")}
+        </>
 
-    return null;
+    );
 };
 
 
 export default ResolveAuthScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// useEffect(() => {
+//     var isMounted = true;
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user != null) {
+//             navigate("Menu");
+//         } else {
+//             navigate("loginFlow");
+//         }
+//     });
+
+//     return () => { isMounted = false; }
+// });
+
+// return null;
