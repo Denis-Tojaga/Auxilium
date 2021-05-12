f559bb440c5e1dadd0852be66a2bc27b661f8ff8
Denis <denis.tojaga@edu.fit.ba>
2021-05-12 17:10:36 +0200

    //resolving local sign in

--- src/screens/ResolveAuthScreen.js
+++ src/screens/ResolveAuthScreen.js
@@ -1,12 +1,30 @@
 import React, { useEffect } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { navigate } from "../helpers/navigation";
+import * as firebase from "firebase";
+
 
 const ResolveAuthScreen = () => {
 
-    // const tryLocalSignIn = 
 
-    // useEffect(() => {
-    //     tryLocalSignin();
-    // }, []);
+
+
+
+    useEffect(() => {
+        var isMounted = true;
+
+        firebase.auth().onAuthStateChanged(user => {
+            if (user != null) {
+                console.log('We are authenticated now!');
+                navigate("Menu");
+            } else {
+                navigate("loginFlow");
+            }
+
+        });
+
+        return () => { isMounted = false; }
+    });
 
     return null;
 };
