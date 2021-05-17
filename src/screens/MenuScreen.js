030276daca86e80c002b43693af3520d161b0329
Denis <denis.tojaga@edu.fit.ba>
2021-05-17 12:33:48 +0200

    //animation finished

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,32 +1,46 @@
 import React, { useRef, useState, useEffect } from "react";
-import { StyleSheet, Text, View, Dimensions, Animated, FlatList, TouchableOpacity, Image } from "react-native";
-import { PanGestureHandler } from "react-native-gesture-handler";
-import { navigate } from "../helpers/navigation";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    Animated,
+    FlatList,
+    TouchableOpacity,
+    Image,
+    Easing,
+    SafeAreaViewBase,
+    SafeAreaView
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import MenuCard from "../components/MenuCard";
+import { navigate } from "../helpers/navigation";
 import * as firebase from "firebase";
 import "firebase/firestore";
 import { Ionicons } from '@expo/vector-icons';
 
 
-
-var WIDTH = Dimensions.get('window').width;
-var HEIGHT = Dimensions.get('window').height;
+const { width, height } = Dimensions.get("screen");
 
 
+
+const PHOBIA_IMAGE_HEIGHT = 125;
+const ITEM_SIZE = height * 0.25 + 50;
 
 
 
 const MenuScreen = () => {
 
+    //animation managing
+    const scrollY = useRef(new Animated.Value(0)).current;
+
+
+
+
+
     const [phobias, setPhobias] = useState([]);
-
-
-
-    //this is called when screen loads for the first time
-    //seting the state to an array of all document inside "phobias" collection
     useEffect(() => {
-
+        //this is called when screen loads for the first time
+        //seting the state to an array of all document inside "phobias" collection
         var newArray = [];
         var db = firebase.firestore();
         db.collection("phobias").get().then((querySnapshot) => {
@@ -39,8 +53,13 @@ const MenuScreen = () => {
 
 
 
+
+
+
+
     return (
         <LinearGradient start={[-0.6, -0.3]} end={[0.8, 0.5]} colors={["#0E0E0E", "#0F2F6A"]} style={styles.container1} >
+
             {/*HEADER CONTAINER*/}
             <View style={styles.headerContainer}>
                 <Text style={styles.header}>Choose your phobia</Text>
@@ -55,31 +74,61 @@ const MenuScreen = () => {
 
             {/*LIST CONTAINER*/}
             <View style={styles.container2}>
-                <FlatList data={phobias}
+
+                <Animated.FlatList
+                    onScroll={Animated.event(
+                        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                        { useNativeDriver: true }
+                    )}
+                    data={phobias}
                     showsVerticalScrollIndicator={false}
                     keyExtractor={(item) => item.id}
-                    renderItem={({ item }) => {
+                    renderItem={({ item, index }) => {
+
+                        // FlatList Animation Reference code
+                        const inputRange = [
+                            -1,
+                            0,
+                            ITEM_SIZE * index,
+                            ITEM_SIZE * (index + 1.4)
+                        ]
+                        const opacityInputRange = [
+                            -1,
+                            0,
+                            ITEM_SIZE * index,
+                            ITEM_SIZE * (index + 0.9)
+                        ]
+                        const scale = scrollY.interpolate({
+                            inputRange,
+                            outputRange: [1, 1, 1, 0]
+                        })
+                        const opacity = scrollY.interpolate({
+                            inputRange: opacityInputRange,
+                            outputRange: [1, 1, 1, 0]
+                        })
+                        // FlatList Animation Reference code
+
+
                         return (
-                            // <MenuCard phobiaID={item.id} />
-                            <LinearGradient start={[0.5, 0.2]} end={[0.5, 1.1]} colors={["#408BC0", "#FAFAFA"]} style={styles.card} >
+                            <Animated.View style={{ width: width * 0.85, height: height * 0.25, marginTop: 25, borderRadius: 25, transform: [{ scale }], opacity }}>
 
-                                <Text style={styles.title}>{item.data.name}</Text>
-                                <Text style={styles.description}>{item.data.description}</Text>
+                                <LinearGradient start={[0.5, 0.2]} end={[0.5, 1.1]} colors={["#408BC0", "#FAFAFA"]} style={styles.gradient} >
+                                    <Text style={styles.title}>{item.data.name}</Text>
+                                    <Text style={styles.description}>{item.data.description}</Text>
 
-                                <Image style={styles.image} source={{ uri: item.data.url }} />
+                                    <Image style={styles.image} source={{ uri: item.data.url }} />
 
-                                <TouchableOpacity style={styles.button}>
-                                    <Text style={styles.buttonText}>Enroll</Text>
-                                    <Ionicons name="ios-play" style={styles.icon} />
-                                </TouchableOpacity>
+                                    <TouchableOpacity style={styles.button}>
+                                        <Text style={styles.buttonText}>Enroll</Text>
+                                        <Ionicons name="ios-play" style={styles.icon} />
+                                    </TouchableOpacity>
+                                </LinearGradient>
 
-                            </LinearGradient>
+                            </Animated.View>
                         );
                     }}
                 />
             </View>
-
-
         </LinearGradient>
     );
 
@@ -94,25 +143,23 @@ const styles = StyleSheet.create({
     /*Containers */
     container1: {
         backgroundColor: "black",
-        width: WIDTH,
-        height: HEIGHT,
+        width: width,
+        height: height,
         alignItems: "flex-end"
     },
-
     headerContainer: {
-        width: WIDTH,
-        height: HEIGHT * 0.35,
+        width: width,
+        height: height * 0.35,
         padding: 20,
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
 
     },
-
     container2: {
         backgroundColor: "#FFFF",
-        width: WIDTH,
-        height: HEIGHT * 0.65,
+        width: width,
+        height: height * 0.65,
         borderTopRightRadius: 45,
         borderTopLeftRadius: 45,
         borderColor: "black",
@@ -122,7 +169,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
         paddingTop: 5
     },
-
 
 
     /*Header */
@@ -150,16 +196,13 @@ const styles = StyleSheet.create({
 
 
 
-    /*MENU CARD */
-    card: {
-        width: WIDTH * 0.85,
-        height: HEIGHT * 0.25,
-        backgroundColor: "blue",
-        marginTop: 25,
+    /*MENU CARDS STYLES */
+    gradient: {
+        width: "100%",
+        height: "100%",
         borderRadius: 25,
-        paddingLeft: 15
+        paddingLeft: 15,
     },
-
     title: {
         alignSelf: "flex-start",
         marginTop: 40,
@@ -167,24 +210,18 @@ const styles = StyleSheet.create({
         fontFamily: "TrendaSemibold",
         color: "#0E0E0E"
     },
-
-
-
     description: {
         fontSize: 20,
         fontFamily: "TrendaLight",
         color: "#0E0E0E"
     },
-
-
     image: {
         width: 65,
-        height: 130,
+        height: PHOBIA_IMAGE_HEIGHT,
         position: "absolute",
         right: 15,
-        bottom: 25
+        bottom: 30
     },
-
     button: {
         width: 90,
         height: 35,
@@ -195,21 +232,16 @@ const styles = StyleSheet.create({
         alignItems: "center",
         marginTop: 5,
     },
-
     buttonText: {
         fontFamily: "MoonBold",
         fontSize: 11,
         color: "#FFFF"
     },
-
     icon: {
         fontSize: 17,
         color: "#FFFF",
         marginLeft: 3
     }
-
-
-
 });
 
 
